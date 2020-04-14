959823be0c4bba3d8b42088a43456f7a105f3669
natureks <sekkappa@gmail.com>
2020-04-14 18:04:55 -0400

    AWS S3 Loader updated

--- LoadS3.py
+++ LoadS3.py
@@ -5,8 +5,9 @@ from config import awskey
 from config import secret
 
 class AwsS3Loader():
-    def LoadFile(bucketname, file):
+    def LoadAsDf(bucketname, file):
         s3 = boto3.client('s3',aws_access_key_id=awskey,aws_secret_access_key=secret)
         Object_Read=s3.get_object(Bucket=bucketname,Key=file)
         output_data= pd.read_csv(Object_Read['Body'])
-        return output_data
+        df = pd.DataFrame(output_data)
+        return df
