9a34cbb530135740e51a3f389f91db98bc5e3d3e
Jackie <kamprat2@gmail.com>
2020-04-16 20:36:37 -0400

    navbar

---
+++ js/heatmap.js
@@ -0,0 +1,14 @@
+////Crime Heatmap
+// Creating map object
+var myMap = L.map("chart1", {
+    center: [33.7490, -84.3880],
+    zoom: 8
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
