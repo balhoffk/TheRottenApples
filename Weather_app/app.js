98d87978da12afc73629951bf9d41daf6b3a755e
geleigh <57293957+geleigh@users.noreply.github.com>
2020-04-24 11:50:23 -0400

    almost done
    almost done

--- Weather_app/app.js
+++ Weather_app/app.js
@@ -5,13 +5,14 @@ window.addEventListener('load',()=>{
     let temperatureDiscription = document.querySelector('.temperature-description');
     let timezoneDisplay = document.querySelector(".location-timezone");
     let iconElement = document.querySelector('.icon-weather');
+    let fDisplay = document.querySelector('.fcast');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
             // Using open weather API
-            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=08e5e0726d2dd6ba61dfc61132143db1&units=imperial`;
+            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=33.7&lon=-84.3&appid=08e5e0726d2dd6ba61dfc61132143db1&units=imperial`;
             // Fecthing API data
             fetch(api)
                 .then(response => {
@@ -20,20 +21,28 @@ window.addEventListener('load',()=>{
                 .then(data => {
                     
                     const {temp} = data.current;
-                    const {timezone} = data;
+                    
+                    // const {timezone} = data;
                     // Setting up the DOM elements.
                     temperatureDisplay.textContent = temp;
                     temperatureDiscription.textContent = data.current.weather[0].description.toUpperCase();
-                    timezoneDisplay.textContent = timezone;
+                    //timezoneDisplay.textContent = timezone;
                     // console.log(data) to see the data and explore it.
                     // calling funtion to set Icon with respect to Open Weather API icon
                     setIcon(data.current.weather[0].icon)
+
+                    function setIcon(iconID){
+                        const iconSrc = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+                        iconElement.setAttribute('src',iconSrc);
+                    }
+                    forecasted_crime = Math.round(temp*.83)                   
+                    fDisplay.textContent = forecasted_crime
                         })
                     })
                 };
         });
-    function setIcon(iconID){
-        const iconSrc = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
-        iconElement.setAttribute('src',iconSrc);
-    }
+    //function setIcon(iconID){
+      //  const iconSrc = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+        //iconElement.setAttribute('src',iconSrc);
+    //}
 ;
